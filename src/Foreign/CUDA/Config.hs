0b8c68572179e9f12b86db7380caa5fa9dbf1db8
Trevor L. McDonell <trevor.mcdonell@gmail.com>
2019-09-27 17:33:59 +0200

    initial commit

---
+++ src/Foreign/CUDA/Config.hs
@@ -0,0 +1,11 @@
+-- Module      : Foreign.CUDA.Config
+-- Copyright   : [2019] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <trevor.mcdonell@gmail.com>
+-- Stability   : experimental
+-- Portability : unknown
+--
+
+module Foreign.CUDA.Config where
+
